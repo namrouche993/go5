64ee3006ee0a5236e2450deae9b86985d6b1c248
namrouche993 <42832704+namrouche993@users.noreply.github.com>
2021-12-02 23:45:32 +0100

    Update app_server.R

--- R/app_server.R
+++ R/app_server.R
@@ -7,11 +7,11 @@
 app_server <- function( input, output, session ) {
   # Your application server logic
   output$mtable1<-renderTable({
-    head(go3::m1)
+    head(go5::m1)
   })
 
   output$m333<-renderTable({
-    go3::m3
+    go5::m3
   })
 
   output$m4441<-renderTable({
